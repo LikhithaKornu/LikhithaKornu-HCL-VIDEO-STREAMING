f2f08a43df2a0f3f060c39a5201258961c8b1f37
LikhithaKornu <likhithakornu03@gmail.com>
2024-10-10 12:24:11 +0530

    Update CPM.cmake

--- cmake/CPM.cmake
+++ cmake/CPM.cmake
@@ -5,7 +5,7 @@
 # MIT License
 # -----------
 #[[
-  Copyright (c) 2021 Lars Melchior and additional contributors
+  Copyright (c) 2024 LikhithaKornu and additional contributors
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
